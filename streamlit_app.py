4638011e9988bb2bbdb87fc0cc246987f54cfca9
Prashanth Raghavan <praghavan@netflix.com>
2023-10-10 16:05:43 +0530

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,8 @@ import streamlit as st
 from hugchat import hugchat
 from hugchat.login import Login
 
-st.title('AI Chatbot')
-
 st.set_page_config(page_title="🤗💬 HugChat")
+st.title('AI Chatbot')
 
 # Hugging face credentials
 
